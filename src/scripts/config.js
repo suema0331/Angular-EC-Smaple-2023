f5c081ec488f8236c20f4525ce0cc7af541b2891
Haruno Suematsu <haruno.s31203@gmail.com>
2023-01-13 18:57:28 +0900

    add scripts

---
+++ src/scripts/config.js
@@ -0,0 +1,14 @@
+// Firebase Config
+const firebaseConfig = {
+    projectId: 'my-ec-2023',
+    appId: '1:995863562978:web:cae439582538e8787decbb',
+    storageBucket: 'my-ec-2023.appspot.com',
+    apiKey: 'AIzaSyCVmuRmHUGT2j0w6XxtsP5aAyvIi2ilSa0',
+    authDomain: 'my-ec-2023.firebaseapp.com',
+    databaseURL: "https://my-ec-2023-default-rtdb.asia-southeast1.firebasedatabase.app",
+    messagingSenderId: '995863562978',
+    measurementId: 'G-NT4TSNGS8E',
+}
+// Exports
+module.exports = firebaseConfig;
+
