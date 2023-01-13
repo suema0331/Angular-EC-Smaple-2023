5a3595d2f1002ad828e5985e8a671cb7bf0eb51f
Haruno Suematsu <haruno.s31203@gmail.com>
2023-01-13 18:57:28 +0900

    add scripts

---
+++ src/scripts/import-datal.js
@@ -0,0 +1,24 @@
+const firebaseConfig = require('./config.js');
+const serviceAccount = require('./serviceAccount.json');
+const { initializeFirebaseApp, restore } = require('firestore-export-import')
+
+initializeFirebaseApp(serviceAccount,firebaseConfig.appName)
+
+// JSON To Firestore
+const jsonToFirestore = async () => {
+  try {
+    console.log('Initialzing Firebase');
+    console.log('Firebase Initialized');
+
+    restore('./data/products.json', {
+      autoParseDates: true, // timestamp → Date
+    });
+    console.log('Upload Success');
+  }
+  catch (error) {
+    console.log(error);
+  }
+};
+
+jsonToFirestore();
+
