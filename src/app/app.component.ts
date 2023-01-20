419772d7a9b672d461ad275ea4fc33fe587997d6
Haruno Suematsu <haruno.s31203@gmail.com>
2023-01-20 18:24:40 +0900

    add: PWA

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,9 @@
-import { Component } from '@angular/core';
+import { Platform } from '@angular/cdk/platform';
+import { Component, HostListener } from '@angular/core';
 import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
 import { Subscription, filter, map, mergeMap } from 'rxjs';
+import { StorageService } from 'src/shared/services/storage.service';
+import { STORAGE_KEY_CLOSED_PWA_DL_MODAL } from './extra/constants';
 import { MetaInfo, SEOService } from './service/utilities/seo.service';
 
 @Component({
@@ -15,10 +18,15 @@ export class AppComponent {
 
   routerEventSubscription?: Subscription;
 
+  modalPwaEvent: any;
+  modalPwaPlatform: string | undefined;
+
   constructor(
     private router: Router,
     private seoService: SEOService,
-    private activatedRoute: ActivatedRoute
+    private activatedRoute: ActivatedRoute,
+    private platform: Platform,
+    private storageService: StorageService
   ) {}
 
   ngOnInit(): void {
@@ -48,9 +56,73 @@ export class AppComponent {
           ? this.seoService.updateOgUrl(String(event.ogUrl))
           : this.seoService.updateOgUrl('');
       });
+    const hasShowOnboard = this.storageService.get(
+      STORAGE_KEY_CLOSED_PWA_DL_MODAL
+    );
+    console.log(hasShowOnboard);
+    if (!hasShowOnboard || hasShowOnboard === 'false') {
+      this.loadModalPwa();
+    }
   }
 
   ngOnDestroy(): void {
     this.routerEventSubscription?.unsubscribe();
+  }
+
+  @HostListener('window:beforeinstallprompt', ['$event'])
+  onbeforeinstallprompt($event: any) {
+    console.log($event);
+    // Prevent Chrome 67 and earlier from automatically showing the prompt
+    $event.preventDefault();
+    // Stash the event so it can be triggered later.
+    this.modalPwaEvent = $event;
+    console.log("🔥beforeinstallprompt' event was fired.");
+  }
+
+  private loadModalPwa(): void {
+    console.log(this.platform);
+    const isInStandaloneMode =
+      'standalone' in window.navigator && (<any>window.navigator)['standalone']; // true: working on PWA
+    console.log(isInStandaloneMode);
+    if (this.platform.ANDROID) {
+      this.modalPwaPlatform = 'ANDROID';
+    }
+
+    // if (this.platform.IOS && this.platform.SAFARI) {
+    if (this.platform.IOS) {
+      if (!isInStandaloneMode) {
+        this.modalPwaPlatform = 'IOS';
+      }
+    }
+  }
+
+  public addToHomeScreen(): void {
+    console.log(this.modalPwaEvent);
+    try {
+      this.modalPwaEvent.prompt();
+      // Wait for the user to respond to the prompt
+      // this.modalPwaEvent.userChoice.then((res: any) => {
+      //   if (res.outcome === 'accepted') {
+      //     console.log('User accepted the A2HS prompt');
+      //   } else {
+      //     console.log('User dismissed the A2HS prompt');
+      //   }
+      //   this.modalPwaEvent = null;
+      // });
+    } catch (error: any) {
+      console.log('error.message');
+      console.log(error.message);
+      alert(
+        `🥲Sorry, something happened. Sorry, something seems to have happened. You may have already added it, or you can manually press the "Add to Home Screen" button in the menu.`
+      );
+      this.storageService.set(STORAGE_KEY_CLOSED_PWA_DL_MODAL, 'true');
+    }
+    this.modalPwaEvent = null;
+    this.modalPwaPlatform = undefined;
+  }
+
+  public closePwa(): void {
+    this.modalPwaPlatform = undefined;
+    this.storageService.set(STORAGE_KEY_CLOSED_PWA_DL_MODAL, 'true');
   }
 }
--- src/app/extra/constants.ts
+++ src/app/extra/constants.ts
@@ -4,7 +4,11 @@ export const STORE_ID_SAMPLE = '1001';
 // Maximum number of products to be purchased
 export const CONSTRAINT_MAX = 50;
 
+// Storage key for user cart cache
 export const STORAGE_KEY_CART = 'user_storage_cart';
 
-// Registration Completion Flag
+//  Storage key for shown onboard modal flag
 export const STORAGE_KEY_SHOWN_ONBOARD = 'shown_onboard';
+
+// Storage key for shown pwa dl modal flag
+export const STORAGE_KEY_CLOSED_PWA_DL_MODAL = 'closed_pwa_dl_modal';
