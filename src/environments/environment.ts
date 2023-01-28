9aa32c1aa831fc622efdfe084438a72886a53fbc
Haruno Suematsu <haruno.s31203@gmail.com>
2023-01-29 00:12:04 +0900

    refactor-isLoggedIn

---
+++ src/environments/environment.ts
@@ -0,0 +1,14 @@
+export const environment = {
+   production: false,
+   remote: false,
+   firebase: {
+      projectId: "",
+      appId: "",
+      storageBucket: "",
+      apiKey: "",
+      authDomain: "",
+      databaseURL: "",
+      messagingSenderId: "",
+      measurementId: ""
+   }
+};
