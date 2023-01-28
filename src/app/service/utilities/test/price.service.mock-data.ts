f7e0a01815e611f25f57550152f9474984a280c7
Haruno Suematsu <haruno.s31203@gmail.com>
2023-01-28 10:05:15 +0900

    add: unit testing and workflows

---
+++ src/app/service/utilities/test/price.service.mock-data.ts
@@ -0,0 +1,118 @@
+export const MOCK_DATA = {
+  products: [
+    {
+      store_product_id: '1',
+      product_name: 'Apple',
+      standard_price: 999,
+      store_price: 99,
+      constraint_max: 10,
+      producing_area: 'Germany',
+      brand: 'Good-brand',
+      internal_capacity: '1 bag',
+      unit_range: '2~3 pcs.',
+      store_comment: 'Very Good Taste!',
+      tags: ['NEW', 'FRESH', 'NICE PRICE', 'LIMITED'],
+      product_images: [
+        {
+          small: '/assets/product/small/44.jpg',
+          master: '/assets/product/master/44.jpg',
+        },
+      ],
+      purchased_flag: 0,
+      favorite_flag: 1,
+      product_status: 1,
+      cart_quantity: 0,
+      product_view_image_list: [
+        {
+          small: '/assets/product/small/44.jpg',
+          master: '/assets/product/master/44.jpg',
+        },
+        {
+          small: '/assets/product/small/17.jpg',
+          master: '/assets/product/master/17.jpg',
+        },
+        {
+          small: '/assets/product/small/18.jpg',
+          master: '/assets/product/master/18.jpg',
+        },
+        {
+          small: '/assets/product/small/25.jpg',
+          master: '/assets/product/master/25.jpg',
+        },
+        {
+          small: '/assets/product/small/22.jpg',
+          master: '/assets/product/master/22.jpg',
+        },
+        {
+          small: '/assets/product/small/21.jpg',
+          master: '/assets/product/master/21.jpg',
+        },
+        {
+          small: '/assets/product/small/29.jpg',
+          master: '/assets/product/master/29.jpg',
+        },
+        {
+          small: '/assets/product/small/35.jpg',
+          master: '/assets/product/master/35.jpg',
+        },
+      ],
+    },
+    {
+      store_product_id: '2',
+      product_name: 'Osterkuchen',
+      standard_price: 100,
+      store_price: 90,
+      constraint_max: 10,
+      producing_area: 'Germany',
+      brand: 'Great-brand',
+      internal_capacity: '1 bag',
+      unit_range: '2~3 pcs.',
+      store_comment: 'Very Good Taste!',
+      tags: ['NEW', 'NICE', 'LIMITED'],
+      product_images: [
+        {
+          small: '/assets/product/small/6.jpg',
+          master: '/assets/product/master/6.jpg',
+        },
+      ],
+      purchased_flag: 0,
+      favorite_flag: 1,
+      product_status: 1,
+      cart_quantity: 0,
+      product_view_image_list: [
+        {
+          small: '/assets/product/small/6.jpg',
+          master: '/assets/product/master/6.jpg',
+        },
+        {
+          small: '/assets/product/small/2.jpg',
+          master: '/assets/product/master/2.jpg',
+        },
+        {
+          small: '/assets/product/small/21.jpg',
+          master: '/assets/product/master/21.jpg',
+        },
+        {
+          small: '/assets/product/small/29.jpg',
+          master: '/assets/product/master/29.jpg',
+        },
+        {
+          small: '/assets/product/small/35.jpg',
+          master: '/assets/product/master/35.jpg',
+        },
+        {
+          small: '/assets/product/small/7.jpg',
+          master: '/assets/product/master/7.jpg',
+        },
+        {
+          small: '/assets/product/small/8.jpg',
+          master: '/assets/product/master/8.jpg',
+        },
+        {
+          small: '/assets/product/small/9.jpg',
+          master: '/assets/product/master/9.jpg',
+        },
+      ],
+    },
+  ],
+};
--- src/backend/services/system.status.rest.user.service.ext.ts
+++ src/backend/services/system.status.rest.user.service.ext.ts
@@ -1,17 +1,17 @@
-import { Injectable } from '@angular/core';
+// import { Injectable } from '@angular/core';
 
-@Injectable({
-  providedIn: 'root',
-})
-export class SystemStatusRestUserServiceExt {
-  // constructor() {} // private httpClient: RestClient
-  /**
-   * Services for the Presentation layer.
-   * Sample of throwing a request from the front end when a backend server exists
-   */
-  // Get the status of the entire system, including multiple applications, such as the user application, admin application.
-  // getSystemStatus(): Observable<SystemStatusResponse> {
-  //   const endpoint = `/api/user/system_configurations_ext/system_status_ext`;
-  //   return this.httpClient.get<SystemStatusResponse>(endpoint);
-  // }
-}
+// @Injectable({
+//   providedIn: 'root',
+// })
+// export class SystemStatusRestUserServiceExt {
+//   // constructor() {} // private httpClient: RestClient
+//   /**
+//    * Services for the Presentation layer.
+//    * Sample of throwing a request from the front end when a backend server exists
+//    */
+//   // Get the status of the entire system, including multiple applications, such as the user application, admin application.
+//   // getSystemStatus(): Observable<SystemStatusResponse> {
+//   //   const endpoint = `/api/user/system_configurations_ext/system_status_ext`;
+//   //   return this.httpClient.get<SystemStatusResponse>(endpoint);
+//   // }
+// }
